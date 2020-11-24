609a469a09ed40fe7e034e8330e7656403d4cf54
Antonio Lopez <36496887+antonio-lopez@users.noreply.github.com>
2020-11-23 19:22:20 -0800

    rewrote game using tic-tac-toe template

--- connect4/src/index.js
+++ connect4/src/index.js
@@ -1,195 +1,288 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+function Square(props) {
+    return (
+      <button className={cName(props.value, props.highlight)} onClick={props.onClick}>
+        
+      </button>
+    );
+  }
+ 
+class Board extends React.Component {
+  renderSquare(i) {
+    const winLine = this.props.winLine;
+    return (
+      <Square key={i}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+        highlight={winLine && winLine.includes(i)}
+      />
+    );
+  }
 
-class App extends React.Component {
-    constructor(props) {
-      super(props);
-      
-      this.state = {
-        player1: 1,
-        player2: 2,
-        currentPlayer: null,
-        board: [],
-        gameOver: false,
-        message: ''
-      };
-      
-      // Bind play function to App component
-      this.play = this.play.bind(this);
-    }
-    
-    // Starts new game
-    initBoard() {
-      // Create a blank 6x7 matrix
-      let board = [];
-      for (let r = 0; r < 6; r++) {
-        let row = [];
-        for (let c = 0; c < 7; c++) { row.push(null) }
-        board.push(row);
+  render() {
+    const horSize = 7;
+    const verSize = 6;
+    let squares = [];
+    let row = [];
+
+    for (let i = 0; i < verSize; i++)
+    {
+      squares = [];
+      for (let j = 0; j < horSize; j++) {
+        let size = i * horSize + j;
+        squares.push(this.renderSquare(size))
       }
-      
-      this.setState({
-        board,
-        currentPlayer: this.state.player1,
-        gameOver: false,
-        message: ''
-      });
+      row.push(<div key={i} className="board-row">{squares}</div>)
     }
-    
-    togglePlayer() {
-      return (this.state.currentPlayer === this.state.player1) ? this.state.player2 : this.state.player1;
-    }
-    
-    play(c) {
-      if (!this.state.gameOver) {
-        // Place piece on board
-        let board = this.state.board;
-        for (let r = 5; r >= 0; r--) {
-          if (!board[r][c]) {
-            board[r][c] = this.state.currentPlayer;
-            break;
-          }
-        }
+    return (<div> {row} </div> );
+  }
+}
+
+function cName(val1, val2) {
+  if (val2) {
+    return 'square highlight'
+  } else if (val1 === 'Red') {
+    return 'square red';
+  } else if (val1 === 'Yellow') {
+    return 'square yellow';
+  } else {
+    return 'square';
+  }
+}
   
-        // Check status of board
-        let result = this.checkAll(board);
-        if (result === this.state.player1) {
-          this.setState({ board, gameOver: true, message: 'Player 1 (red) wins!' });
-        } else if (result === this.state.player2) {
-          this.setState({ board, gameOver: true, message: 'Player 2 (yellow) wins!' });
-        } else if (result === 'draw') {
-          this.setState({ board, gameOver: true, message: 'Draw game.' });
-        } else {
-          this.setState({ board, currentPlayer: this.togglePlayer() });
-        }
-      } else {
-        this.setState({ message: 'Game over. Please start a new game.' });
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(42).fill(null)
+      }],
+      stepNumber: 0,
+      xIsNext: true,
+      isAscending: true,
+      x: 0,
+      y: 0
+    };
+  }
+
+  handleClick(i) {
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
+      const current = history[history.length - 1];
+      let squares = current.squares.slice();
+      squares[i] = this.state.xIsNext ? 'Red' : 'Yellow';
+      this.setState({
+        history: history.concat([{
+          squares: squares,
+          squareX: 1 + i % 7,
+          squareY: 1 + Math.floor(i / 6)
+        }]),
+        stepNumber: history.length,
+        xIsNext: !this.state.xIsNext,
+      });
+      if (calculateWinner(squares).winner) {
+        return;
       }
     }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    });
+  }
+
+  sortSwitch() {
+    this.setState({
+      isAscending: !this.state.isAscending
+    });
+  }
+
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winObj = calculateWinner(current.squares);
+    const winner = winObj.winner;
     
-    checkVertical(board) {
-      // Check only if row is 3 or greater
-      for (let r = 3; r < 6; r++) {
-        for (let c = 0; c < 7; c++) {
-          if (board[r][c]) {
-            if (board[r][c] === board[r - 1][c] &&
-                board[r][c] === board[r - 2][c] &&
-                board[r][c] === board[r - 3][c]) {
-              return board[r][c];    
-            }
-          }
-        }
-      }
-    }
-    
-    checkHorizontal(board) {
-      // Check only if column is 3 or less
-      for (let r = 0; r < 6; r++) {
-        for (let c = 0; c < 4; c++) {
-          if (board[r][c]) {
-            if (board[r][c] === board[r][c + 1] && 
-                board[r][c] === board[r][c + 2] &&
-                board[r][c] === board[r][c + 3]) {
-              return board[r][c];
-            }
-          }
-        }
-      }
-    }
-    
-    checkDiagonalRight(board) {
-      // Check only if row is 3 or greater AND column is 3 or less
-      for (let r = 3; r < 6; r++) {
-        for (let c = 0; c < 4; c++) {
-          if (board[r][c]) {
-            if (board[r][c] === board[r - 1][c + 1] &&
-                board[r][c] === board[r - 2][c + 2] &&
-                board[r][c] === board[r - 3][c + 3]) {
-              return board[r][c];
-            }
-          }
-        }
-      }
-    }
-    
-    checkDiagonalLeft(board) {
-      // Check only if row is 3 or greater AND column is 3 or greater
-      for (let r = 3; r < 6; r++) {
-        for (let c = 3; c < 7; c++) {
-          if (board[r][c]) {
-            if (board[r][c] === board[r - 1][c - 1] &&
-                board[r][c] === board[r - 2][c - 2] &&
-                board[r][c] === board[r - 3][c - 3]) {
-              return board[r][c];
-            }
-          }
-        }
-      }
-    }
-    
-    checkDraw(board) {
-      for (let r = 0; r < 6; r++) {
-        for (let c = 0; c < 7; c++) {
-          if (board[r][c] === null) {
-            return null;
-          }
-        }
-      }
-      return 'draw';    
-    }
-    
-    checkAll(board) {
-      return this.checkVertical(board) || this.checkDiagonalRight(board) || this.checkDiagonalLeft(board) || this.checkHorizontal(board) || this.checkDraw(board);
-    }
-    
-    componentWillMount() {
-      this.initBoard();
-    }
-    
-    render() {
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        ('Go to move #' + move + ' (' + step.squareX + ', ' + step.squareY + ')') :
+        'Go to game start';
       return (
-        <div>
-          <div className="button" onClick={() => {this.initBoard()}}>New Game</div>
-          
-          <table>
-            <thead>
-            </thead>
-            <tbody>
-              {this.state.board.map((row, i) => (<Row key={i} row={row} play={this.play} />))}
-            </tbody>
-          </table>
-          
-          <p className="message">{this.state.message}</p>
-        </div>
+        <li key={move}>
+          <button className={move === this.state.stepNumber ? 'last-move' : ''}
+            onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
       );
+    });
+
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else if (!current.squares.includes(null)) {
+      status = 'DRAW';
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'Red' : 'Yellow');
+    }
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+            winLine={winObj.winningline}
+          />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <button onClick={() => this.sortSwitch()}>
+            Sort: {this.state.isAscending ? 'ascending' : 'descending'}
+          </button>
+          <ol>{+ this.state.isAscending ? moves : moves.reverse()}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+  
+  // ========================================
+  
+ReactDOM.render( <Game />, document.getElementById('root'));
+
+function checkHorizontal(squares) {
+  let a = 0, b = 1, c = 2, d = 3;
+  let arr = [a, b, c, d];
+  for (let j = 0; j < 6; j++)
+  {
+    for (let i = 0; i < 4; i++){
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c] && squares[a] === squares[d]){
+        arr = [a, b, c, d];
+        return {
+          winning: true,
+          winner: squares[a],
+          winningline: arr,
+      }
+    }
+    a++; b++; c++; d++;
+  }
+  a+=3; b+=3; c+=3; d+=3;
+  }
+  return {
+    winning: false,
+    winner: null,
+    winningline: null,
+  }
+}
+
+function checkVertical(squares) {
+  for(let j = 0; j < 7; j++)
+  {
+    let a = 0, b = 7, c = 14, d = 21;
+    let arr = [a, b, c, d];
+    a+=j; b+=j; c+=j; d+=j
+    for(let i = 0; i < 3; i++)
+    {
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c] && squares[a] === squares[d]){
+        arr = [a, b, c, d];
+      return {
+        winning: true,
+        winner: squares[a],
+        winningline: arr,
+      }
+    }
+    a+=7; b+=7; c+=7; d+=7;
+  }
+}
+return {
+  winning: false,
+  winner: null,
+  winningline: null,
+}
+}
+
+function checkDiagR(squares) {    
+  let a = 0, b = 8, c = 16, d = 24;
+  let arr = [a, b, c, d];
+  for(let j = 0; j < 4; j++)
+  {
+    for(let i = 0; i < 4; i++)
+    {
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c] && squares[a] === squares[d]){
+        arr = [a, b, c, d];
+      return {
+        winning: true,
+        winner: squares[a],
+        winningline: arr,
+      }
+    }
+    a++; b++; c++; d++;
+  }
+  a+=3; b+=3; c+=3; d+=3;
+}
+return {
+  winning: false,
+  winner: null,
+  winningline: null,
+}
+}
+
+function checkDiagL(squares) {
+  let a = 3, b = 9, c = 15, d = 21;
+  let arr = [a, b, c, d];
+  for(let j = 0; j < 4; j++) {
+    for(let i = 0; i < 4; i++) {
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c] && squares[a] === squares[d]) {
+        arr = [a, b, c, d];
+      return {
+        winning: true,
+        winner: squares[a],
+        winningline: arr,
+      }
+    }
+    a++; b++; c++; d++;
+  }
+  a+=3; b+=3; c+=3; d+=3;
+}
+return {
+  winning: false,
+  winner: null,
+  winningline: null,
+} 
+}
+
+function calculateWinner(squares) {
+  let horizontal = checkHorizontal(squares);
+  let vertical = checkVertical(squares);
+  let diagR = checkDiagR(squares);
+  let diagL = checkDiagL(squares);
+  
+  if (horizontal.winning === true) {
+    return {
+      winner: horizontal.winner,
+      winningline: horizontal.winningline,
     }
   }
-  
-  // Row component
-  const Row = ({ row, play }) => {
-    return (
-      <tr>
-        {row.map((cell, i) => <Cell key={i} value={cell} columnIndex={i} play={play} />)}
-      </tr>
-    );
-  };
-  
-  const Cell = ({ value, columnIndex, play }) => {
-    let color = 'white';
-    if (value === 1) {
-      color = 'red';
-    } else if (value === 2) {
-      color = 'yellow';
+  else if (vertical.winning === true) {
+    return {
+      winner: vertical.winner,
+      winningline: vertical.winningline,
     }
-      
-    return (
-      <td>
-        <div className="cell" onClick={() => {play(columnIndex)}}>
-          <div className={color}></div>
-        </div>
-      </td>
-    );
-  };
-  
-  ReactDOM.render(<App />, document.getElementById('root'));
+  }
+  else if (diagR.winning === true) {
+    return {
+      winner: diagR.winner,
+      winningline: diagR.winningline,
+    }
+  }
+  else if (diagL.winning === true) {
+    return {
+      winner: diagL.winner,
+      winningline: diagL.winningline,
+    }
+  }
+  return {
+    winner: null,
+  }
+}
